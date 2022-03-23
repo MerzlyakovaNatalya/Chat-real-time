9cdab4ab22bab687e4eea86c51c70a27d107f0ad
MerzlyakovaNatalya <m.artem.v@inbox.ru>
2022-03-23 16:31:13 +0300

    add redux

--- src/routers/chat/Chat.js
+++ src/routers/chat/Chat.js
@@ -1,19 +1,20 @@
-import { ChatList } from '../../components/chatList/ChatList';
-import { CHATS } from '../../helpers/mock';
-import style from './Chats.module.css';
-import { Outlet } from 'react-router-dom';
+import { ChatList } from "../../components/chatList/ChatList";
+import { CHATS } from "../../helpers/mock";
+import style from "./Chats.module.css";
+import { Outlet } from "react-router-dom";
 
 export const Chat = () => {
+  useDispatch();
 
-    return (
-      <>
+  return (
+    <>
       <div className={style.wrap}>
-         <h1>CHAT</h1>
-         <div className={style.wrapper}>
-         <ChatList list={CHATS}></ChatList>
-         </div>
-          <Outlet></Outlet>
+        <h1>CHAT</h1>
+        <div className={style.wrapper}>
+          <ChatList list={CHATS}></ChatList>
         </div>
-      </>
-    )
-}
+        <Outlet></Outlet>
+      </div>
+    </>
+  );
+};
