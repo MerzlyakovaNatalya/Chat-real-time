f8aab9ee4ec7a47e2ef46025b1aa142ad9a70bd0
MerzlyakovaNatalya <m.artem.v@inbox.ru>
2022-09-22 10:50:34 +0300

    add22.09

--- src/components/chatFirebase/index.js
+++ src/components/chatFirebase/index.js
@@ -1,17 +1,8 @@
+import React from "react";
 import { Navbar } from "./components/Navbar";
 import { ChatRouter } from "./components/ChatRouter";
-import React, { useContext } from "react";
-import { Context } from "../../App";
-import { useAuthState } from "react-firebase-hooks/auth";
-import Loader from "./components/Loader";
 
 export const ChatFirebase = () => {
-  const { auth } = useContext(Context);
-  const [loading] = useAuthState(auth);
-
-  if (loading) {
-    return <Loader />;
-  }
   return (
     <div>
       <Navbar>Navbar</Navbar>
